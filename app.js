89cf32d8a0c50a9d26587f30af05668c7b7ace2b
a-di-nis <a-di-nis@student.42.fr>
2021-01-14 17:24:15 +0100

    style

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const axios = require('axios');
 const app = express();
 const port = 3000;
 
+app.use(express.static("public")); //to recognize path of local files
+
 app.use(express.urlencoded({extended: true})); 
 app.use(express.json());
 
